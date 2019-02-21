b429e1b6d13b8d445f52a16d3d0f8d396130ce69
Joseph P Geringer <joegeringer@gmail.com>
2019-02-20 21:56:44 -0600

    added blog post listing

--- my-nuxt-app/pages/index.vue
+++ my-nuxt-app/pages/index.vue
@@ -1,9 +1,26 @@
 <template>
   <section class="container">
+
+    <div class="home-page">
+      <section class="intro">
+        <h1>Get the latest tech news!</h1>
+      </section>
+      <section class="featured-posts">
+        <PostPreview 
+          id="1" 
+          thumbnail="https://files.pitchbook.com/website/images/content/Chip_board.png"
+          title="Hello There!"
+          previewText="This is my first post!" />
+        <PostPreview 
+          id="2" 
+          thumbnail="https://images.techhive.com/images/article/2016/11/computerworld_tech_forecast_2017_hottest-tech-skills-for-2017-100692085-large.jpg"
+          title="AI Is Here"
+          previewText="Yet another post about AI" />
+      </section>      
+    </div>
+
     <div>
-      <h1 class="title">Recipes</h1>
-      <h2 class="subtitle">A nuxt.js example</h2>
-      
+      <h4 class="title3">Recipes</h4>      
       <span>
         <h3>Tutorials:</h3>
         <ul>
@@ -34,46 +51,53 @@
 </template>
 
 <script>
+import PostPreview from '@/components/Posts/PostPreview'
+
 export default {
   components: {
+    PostPreview
   }
 }
 </script>
 
-<style>
-h3{
-  margin-bottom: 1rem;
+<style scoped>
+.intro {
+  height: 300px;
+  position: relative;
+  padding: 30px;
+  box-sizing: border-box;
+  background-position: center;
+  background-size: cover;
 }
-span {
-  text-align: left;
+
+.intro h1 {
+  position: absolute;
+  top: 10%;
+  left: 5%;
+  width: 90%;
+  font-size: 1.5rem;
+  color: black;
+  background-color: rgb(211, 211, 211);
+  padding: 10px;
+  border-radius: 10px;
+  box-shadow: 3px 3px 3px black;
+  box-sizing: border-box;
+  border: 1px solid black;
 }
-.container {
-  min-height: 100vh;
+
+@media (min-width: 768px) {
+  .intro h1 {
+    font-size: 2rem;
+  }
+}
+
+.featured-posts {
   display: flex;
-  justify-content: center;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
   align-items: center;
-  text-align: center;
+  justify-content: center;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
